fb71d5083a8b3ac94f5b4b231e7d7e7f9951692a
hermannm <hermann.morkrid@gmail.com>
2021-10-30 13:45:30 +0200

    add increaseCounter multithreading example

--- learning_tasks/task2/Main.scala
+++ learning_tasks/task2/Main.scala
@@ -1,15 +1,21 @@
 object Task2 extends App {
   def initializeThread(function: => Unit): Thread = {
     return new Thread {
-      override def run() = function;
-    };
+      override def run() = function
+    }
   }
 
-  val thread = initializeThread {
-    println("New thread initialized")
-  }
+  private var counter: Int = 0
 
-  thread.start()
-  thread.join()
-  println("New thread joined");
+  def increaseCounter(): Unit = counter += 1
+
+  def printCounter(): Unit = println(counter)
+
+  val thread1 = initializeThread(increaseCounter)
+  val thread2 = initializeThread(increaseCounter)
+  val thread3 = initializeThread(printCounter)
+
+  thread1.start()
+  thread2.start()
+  thread3.start()
 }
