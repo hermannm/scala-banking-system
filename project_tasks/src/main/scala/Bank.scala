5cfbef1257014479ca8d6727ba07286e7d2bd0ab
hermannm <hermann.morkrid@gmail.com>
2021-11-17 17:24:45 +0100

    implement Bank

--- project_tasks/src/main/scala/Bank.scala
+++ project_tasks/src/main/scala/Bank.scala
@@ -1,21 +1,38 @@
 class Bank(val allowedAttempts: Integer = 3) {
 
-    private val transactionsQueue: TransactionQueue = new TransactionQueue()
+    private val transactionQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
 
-    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = ???
-                                                // TODO
-                                                // project task 2
-                                                // create a new transaction object and put it in the queue
-                                                // spawn a thread that calls processTransactions
+    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+        val transaction: Transaction = new Transaction(
+            transactionQueue,
+            processedTransactions,
+            from,
+            to,
+            amount,
+            allowedAttempts
+        );
 
-    private def processTransactions: Unit = ???
-                                                // TOO
-                                                // project task 2
-                                                // Function that pops a transaction from the queue
-                                                // and spawns a thread to execute the transaction.
-                                                // Finally do the appropriate thing, depending on whether
-                                                // the transaction succeeded or not
+        transactionQueue.push(transaction);
+
+        processTransactions;
+    }
+
+    private def processTransactions: Unit = {
+        if (!transactionQueue.isEmpty) {
+            val transaction: Transaction = transactionQueue.pop;
+
+            transaction.run;
+
+            if (transaction.status == TransactionStatus.PENDING) {
+                transactionQueue.push(transaction);
+                processTransactions;
+            } else {
+                processedTransactions.push(transaction);
+                processTransactions;
+            }
+        }
+    }
 
     def addAccount(initialBalance: Double): Account = {
         new Account(this, initialBalance)
