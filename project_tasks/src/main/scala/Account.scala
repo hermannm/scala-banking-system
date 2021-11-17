91fb6b4388026c8ca6d2c2a705afe581a8d87455
sondretb <sondrebt@stud.ntnu.no>
2021-11-17 17:20:44 +0100

    1.3 finished.

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -6,14 +6,14 @@ class Account(val bank: Bank, initialBalance: Double) {
 
     val balance = new Balance(initialBalance)
 
-    // TODO
-    // for project task 1.2: implement functions
-    // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = {
-        balance.amount -= amount
+    def withdraw(amount: Double): Either[Unit, String] = this.synchronized{
+        if (amount < 0) return Right("Cannot withdraw a negative amount")
+        if (amount > this.getBalanceAmount) return Right("Not enough funds in account")
+        Left(balance.amount -= amount)
     }
-    def deposit (amount: Double): Unit = {
-        balance.amount += amount
+    def deposit (amount: Double): Either[Unit, String] = this.synchronized{
+        if (amount < 0) return Right("You cannot deposit a negative amount")
+        Left(balance.amount += amount)
     }
     def getBalanceAmount: Double       = balance.amount
 
