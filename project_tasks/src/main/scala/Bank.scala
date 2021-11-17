52c0f9b3209b67562e4e644dbbd9de0d6475af8e
haved <krogstie.havard@gmail.com>
2021-11-17 17:54:01 +0100

    last things

--- project_tasks/src/main/scala/Bank.scala
+++ project_tasks/src/main/scala/Bank.scala
@@ -3,6 +3,8 @@ class Bank(val allowedAttempts: Integer = 3) {
     private val transactionQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
 
+    private var processing = false;
+
     def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
         val transaction: Transaction = new Transaction(
             transactionQueue,
@@ -13,24 +15,35 @@ class Bank(val allowedAttempts: Integer = 3) {
             allowedAttempts
         );
 
-        transactionQueue.push(transaction);
+        this.synchronized {
+            transactionQueue.push(transaction)
 
-        processTransactions;
+            if (!processing) {
+                processing = true
+                new Thread {
+                    override def run = processTransactions
+                }.start
+            }
+        }
     }
 
     private def processTransactions: Unit = {
-        if (!transactionQueue.isEmpty) {
-            val transaction: Transaction = transactionQueue.pop;
+        while (true) {
+            this.synchronized {
+                if(transactionQueue.isEmpty) {
+                    processing = false
+                    return
+                }
+            }
+            val transaction: Transaction = transactionQueue.pop
 
-            transaction.run;
+            transaction.run
 
             if (transaction.status == TransactionStatus.PENDING) {
-                transactionQueue.push(transaction);
-                processTransactions;
+                transactionQueue.push(transaction)
             } else {
-                processedTransactions.push(transaction);
-                processTransactions;
-            }
+                processedTransactions.push(transaction)
+            }            
         }
     }
 
